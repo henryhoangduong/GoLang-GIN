c335b92e0067ec35dec9125d475fc29b24065234
henryhoangduong <henryhoangduong@gmail.com>
2025-01-27 10:17:56 +0700

    jwt finished

--- JWT/token/token.go
+++ JWT/token/token.go
@@ -1,6 +1,7 @@
 package token
 
 import (
+	"fmt"
 	models "jwt/models"
 	"time"
 
@@ -24,6 +25,35 @@ func GenerateToken(claims *models.JwtClaims, expirationTime time.Time) (string, 
 	return tokenString, nil
 }
 
-func VerifyToken(tokenString, origin string)(bool, *models.JwtClaims){
-    claims := &models.JwtClaims{}
+func VerifyToken(tokenString, origin string) (bool, *models.JwtClaims) {
+	claims := &models.JwtClaims{}
+	token, _ := getTokenFromString(tokenString, claims)
+	if token.Valid {
+		if e := claims.Valid(); e == nil {
+			return true, claims
+		}
+	}
+	return false, claims
+}
+
+func GetClaims(tokenString string) models.JwtClaims {
+	claims := &models.JwtClaims{}
+
+	_, err := getTokenFromString(tokenString, claims)
+	if err == nil {
+		return *claims
+	}
+	return *claims
+}
+
+func getTokenFromString(tokenString string, claims *models.JwtClaims) (*jwt.Token, error) {
+	return jwt.ParseWithClaims(tokenString, claims, func(token *jwt.Token) (interface{}, error) {
+		// Don't forget to validate the alg is what you expect:
+		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, fmt.Errorf("Unexpected signing method: %v", token.Header["alg"])
+		}
+
+		// hmacSampleSecret is a []byte containing your secret, e.g. []byte("my_secret_key")
+		return []byte(jWTPrivateToken), nil
+	})
 }

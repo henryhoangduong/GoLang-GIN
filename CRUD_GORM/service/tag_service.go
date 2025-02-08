214dcb13abf2c0129ee825f3d49447aba744d043
henryhoangduong <henryhoangduong@gmail.com>
2025-02-08 18:44:45 +0700

    tag service

--- CRUD_GORM/service/tag_service.go
+++ CRUD_GORM/service/tag_service.go
@@ -1,1 +1,14 @@
 package service
+
+import (
+	"henry/data/request"
+	"henry/data/response"
+)
+
+type TagService interface {
+	Create(tags request.CreateTagsRequest)
+	Update(tags request.UpdateTagsRequest)
+	Delete(tagsId int)
+	FindById(tagsId int) response.TagsResponse
+	FindAll() []response.TagsResponse
+}
